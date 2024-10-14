e2ee700b544bdf8edcea3175096689b23ff95cf0
NowakRichard <120458703+NowakRichard@users.noreply.github.com>
2024-10-14 10:21:58 +0200

    Add files via upload

---
+++ app/Assets/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'league/iso3166' => array(
+            'pretty_version' => '4.3.1',
+            'version' => '4.3.1.0',
+            'reference' => '11703e0313f34920add11c0228f0dd43ebd10f9a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../league/iso3166',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
